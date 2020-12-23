461e7887c7b2ce8e8a1ac5dfc53253005e2228da
N.Minyaev <valitev@gmail.com>
2020-12-23 12:13:06 +0400

    Добваить алгоритм прорисовки поверхности, надо выделить в отдельный поток и повесит на кнопку

--- graph.py
+++ graph.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from colour import Color
 import time
+import numpy as np
 
 LARGE_FONT = ("Verdana", 12)
 
@@ -45,6 +46,11 @@ class GraphFrame(tk.Frame):
         fig = plt.figure()
         self.ax = fig.add_subplot(111, projection='3d')
 
+        self.x_arr = np.arange(0, 100, 1)
+        self.y_arr = np.arange(0, 100, 1)
+        self.x_arr, self.y_arr = np.meshgrid(self.x_arr, self.y_arr)
+        self.z_arr = np.zeros((100, 100))
+
         red = Color("blue")
         self.colors = list(red.range_to(Color("white"), 170))
 
@@ -59,12 +65,16 @@ class GraphFrame(tk.Frame):
         self.canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
     def update_data(self, x=0, y=0, z=0):
-        multiplicity = 2
+        self.z_arr[x, y] = z
+        multiplicity = 1
         condition_add_point = (x != self.x or y != self.y or z != self.z) and \
                               ((x % multiplicity == 0) or (y % multiplicity == 0) or (z % multiplicity == 0))
+
         if condition_add_point:
-            self.ax.scatter(x, y, z, s=2, c=str(self.colors[z]), marker='8')
-            self.ax.plot([x, self.x], [y, self.y], [z, self.z], c=str(self.colors[z]))
+            # self.ax.scatter(x, y, z, s=2, c=str(self.colors[z]), marker='8')
+            # self.ax.plot([x, self.x], [y, self.y], [z, self.z], c=str(self.colors[z]))
+            # self.ax.plot_trisurf(x_arr, y_arr, z_arr, rstride=2, cstride=1, cmap=plt.cm.get_cmap('Blues_r')) # не работает
+            self.ax.plot_surface(self.x_arr, self.y_arr, self.z_arr, rstride=1, cstride=1, cmap=plt.cm.get_cmap('Blues_r'))
             self.x = x
             self.y = y
             self.z = z
