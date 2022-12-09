93e38e6cfd25cbbc97bf153a09f0e14d177f78a5
N.Minyaev <valitev@gmail.com>
2022-12-09 10:25:17 +0400

    Черновик привязки к особенности

--- controller/core_logic/lapshin_algorithm/binding_probe_to_feature.py
+++ controller/core_logic/lapshin_algorithm/binding_probe_to_feature.py
@@ -1,5 +1,6 @@
 from threading import Event
-from typing import Tuple
+from typing import Tuple, List
+import numpy as np
 
 
 class BindingProbeToFeature:
@@ -20,33 +21,74 @@ class BindingProbeToFeature:
     """
 
     def __init__(self, get_val_func, set_x_func, set_y_func, touching_surface_event: Event, **kwargs):
+        self.local_surface = np.zeros((7, 7))
         self.touching_surface_event = touching_surface_event
         self.set_y_func = set_y_func
         self.set_x_func = set_x_func
         self.get_val_func = get_val_func
         self.delay_between_iterations = 1
         self.stop = True
-        self.z_start_feature_height = None
+        self.z_optimal_height = None  # можно вычислить программно после сканирования области local_surface
         self.current_absolute_coordinates_feature = None
 
-    def bypass_feature(self, start_point: Tuple[int, int, int]) -> None:
-        self.bypass_feature(start_point)
+    def bind_to_feature(self, start_point: Tuple[int, int]) -> None:
+        # сканирование области local_surface
 
-    def bypass_point(self, start_point) -> None:
-        x = start_point[0]
-        y = start_point[1]
-        gen = self.__gen_bypass_point((x, y))
+        self.calc_optimal_height(self.local_surface.copy())
+        start_point = self.find_start_point()
 
-        max_iteration = 20
-        for _ in range(max_iteration):
+        coord = self.bypass_feature(start_point)
+
+        centr_coord = self.centroid(coord)
+
+    def calc_optimal_height(self, surface: np.ndarray) -> None:
+        def recur_clip(arr: np.ndarray, next_to_clip: int):
+            arr = np.clip(arr, 0, next_to_clip)
+            if np.amax(arr) != int(arr.mean()):
+                next_to_clip = recur_clip(arr, next_to_clip - 1)
+            return next_to_clip
+
+        self.z_optimal_height = recur_clip(surface, np.amax(surface)) + 2
+
+    def find_start_point(self) -> Tuple[int, int]:
+        # todo должны находится любые стартовые точки на всех поверхности. Фильтровать пройденные фичи будут заранее, обнуляя внутренние области
+        def recur_search_point(surface: np.ndarray, z: int, x: int, y: int) -> Tuple[int, int]:
             try:
-                x, y = next(gen)
-            except StopIteration:
-                break
-            self.set_x_func(x)
-            self.set_y_func(y)
+                if surface[y, x] < z:
+                    x, y = recur_search_point(surface, z, x + 1, y + 1)
+            finally:
+                return x, y
 
-        # todo если найдена новая точка то можно рекурсивно вызвать еще раз
+        return recur_search_point(self.local_surface, self.z_optimal_height, 0, 0)
+
+    def bypass_feature(self, start_point: Tuple[int, int]) -> list:
+        x_start = start_point[0]
+        y_start = start_point[1]
+        points = []
+
+        while points[-1] == (x_start, y_start):
+            gen = self.__gen_bypass_point(points[-1])
+            max_iteration = 8
+            for _ in range(max_iteration):
+                try:
+                    x, y = next(gen)
+                except StopIteration:
+                    break
+                x_prev, y_prev = self.local_surface[-1]
+                if self.local_surface[y, x] >= self.z_optimal_height \
+                   and self.local_surface[y_prev, x_prev] < self.local_surface[y, x]:
+                    points.append((x, y))
+                    break
+
+        return points
+
+    def centroid(self, vertexes: List[Tuple[int, int], ...]):
+        _x_list = [vertex[0] for vertex in vertexes]
+        _y_list = [vertex[1] for vertex in vertexes]
+        _len = len(vertexes)
+        _x = sum(_x_list) / _len
+        _y = sum(_y_list) / _len
+        return _x, _y
 
     def __gen_bypass_point(self, point: Tuple[int, int]):
         """
@@ -67,4 +109,3 @@ class BindingProbeToFeature:
         for _ in range(2):
             y += 1
             yield x, y
-
--- tests/core_logic/lapshin_algorithm/test_binding_probe_to_feature.py
+++ tests/core_logic/lapshin_algorithm/test_binding_probe_to_feature.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from unittest.mock import MagicMock, call
 from controller.core_logic.lapshin_algorithm.binding_probe_to_feature import BindingProbeToFeature
+from stub_microcontroller.surface_generator import SurfaceGenerator
 
 
 class TestBindingProbeToFeature(TestCase):
@@ -19,7 +20,7 @@ class TestBindingProbeToFeature(TestCase):
         )
 
     def test_bypass_single_point(self) -> None:
-        self.BindingProbeToFeature.bypass_point((3, 3))
+        self.BindingProbeToFeature.bypass_feature((3, 3))
 
         calls = [call(3), call(2), call(2), call(2), call(3), call(4), call(4), call(4)]
         self.BindingProbeToFeature.set_x_func.assert_has_calls(calls)
@@ -27,8 +28,9 @@ class TestBindingProbeToFeature(TestCase):
         calls = [call(4), call(4), call(3), call(2), call(2), call(2), call(3), call(4)]
         self.BindingProbeToFeature.set_y_func.assert_has_calls(calls)
 
+    def test_calc_optimal_height(self) -> None:
+        arr = SurfaceGenerator(20, 20, [(5, 5), (10, 5), (15, 5), (5, 10), (10, 10), (15, 10)]).generate_noise_surface()
 
+        self.BindingProbeToFeature.calc_optimal_height(arr)
 
-
-
-
+        self.assertEqual(21, self.BindingProbeToFeature.z_optimal_height)
